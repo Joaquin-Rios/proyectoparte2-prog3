c5e50fbc94f84c475f611de050e0271a6acbfdc6
FranciscoRolon <frolon@udesa.edu.ar>
2022-06-07 14:35:48 -0300

    Profile

--- proyectoIntegrador/src/screens/Profile/Profile.js
+++ proyectoIntegrador/src/screens/Profile/Profile.js
@@ -1,27 +1,51 @@
 import { View, Text, TouchableOpacity, FlatList, ActivityIndicator, StyleSheet } from 'react-native'
 import React, {Component} from 'react'
-import {db} from '../../firebase/config'
+import {auth, db} from '../../firebase/config'
 
 
-class Profile extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      info:[],
-      loading:true
-    }
-  }
-
-
-  render(){
-    return (
-      <View>
-       <Text>Aqui van los datos del perfil </Text>
-
-        
-      </View>
-    )
-  }
+function Profile(props){
+  return(
+    <View style={style.container}>
+      <Text>{auth.currentUser.email}</Text>
+      <TouchableOpacity onPress={()=> props.route.params.logout()}>
+        <Text style={style.boton}>Cerrar Sesion</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
+
+const style = StyleSheet.create({
+  container:{ 
+      padding: 10,
+      marginTop: 20
+  },
+  field: {
+      height: 20,
+      paddingVertical: 15,
+      paddingHorizontal: 10,
+      borderWidth: 1,
+      borderRadius: 6,
+      borderColor: '#ccc',
+      borderStyle: 'solid',
+      marginVertical: 10
+  },
+  boton:{
+      paddingHorizontal: 10,
+      paddingVertical: 6,
+      textAlign: 'center',
+      backgroundColor: '#28a745',
+      borderRadius: 4,
+      borderWidth: 1,
+      borderStyle: 'solid',
+      borderColor: '#28a745'
+  },
+  texto:{
+      textAlign: 'left',
+      padding: 5,
+      fontFamily: 'Arial'
+
+  }
+});
+ 
 
 export default Profile
